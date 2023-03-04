2f690b39f70abb7a4e655a3ee92bf647e101fc38
dharmender <dharmender.manral@gmail.com>
2023-03-04 10:14:35 +0530

    Modify init api response body

--- lib/features/launcher/data/models/device_details_res.dart
+++ lib/features/launcher/data/models/device_details_res.dart
@@ -4,12 +4,34 @@ part 'device_details_res.g.dart';
 
 @JsonSerializable()
 class DeviceDetailsRes {
-  String languageType;
+  late String languageType;
+  late LabelDetails labelDetails;
 
-  DeviceDetailsRes({required this.languageType});
+  DeviceDetailsRes();
 
   factory DeviceDetailsRes.fromJson(Map<String, dynamic> json) =>
       _$DeviceDetailsResFromJson(json);
 
   Map<String, dynamic> toJson() => _$DeviceDetailsResToJson(this);
+}
+
+
+@JsonSerializable()
+class LabelDetails{
+  @JsonKey(name: 'find_imei')
+  late String findImei;
+  @JsonKey(name: 'get_imei_information')
+  late String getImeiInfo;
+  @JsonKey(name: 'imei_details')
+  late String imeiDetails;
+  @JsonKey(name: 'enter_imei')
+  late String enterImei;
+  @JsonKey(name: 'option_a')
+  late String optionA;
+
+  LabelDetails();
+
+  factory LabelDetails.fromJson(Map<String, dynamic> json) => _$LabelDetailsFromJson(json);
+
+  Map<String, dynamic> toJson() => _$LabelDetailsToJson(this);
 }
--- lib/features/launcher/data/models/device_details_res.g.dart
+++ lib/features/launcher/data/models/device_details_res.g.dart
@@ -7,11 +7,29 @@ part of 'device_details_res.dart';
 // **************************************************************************
 
 DeviceDetailsRes _$DeviceDetailsResFromJson(Map<String, dynamic> json) =>
-    DeviceDetailsRes(
-      languageType: json['languageType'] as String,
-    );
+    DeviceDetailsRes()
+      ..languageType = json['languageType'] as String
+      ..labelDetails =
+          LabelDetails.fromJson(json['labelDetails'] as Map<String, dynamic>);
 
 Map<String, dynamic> _$DeviceDetailsResToJson(DeviceDetailsRes instance) =>
     <String, dynamic>{
       'languageType': instance.languageType,
+      'labelDetails': instance.labelDetails,
+    };
+
+LabelDetails _$LabelDetailsFromJson(Map<String, dynamic> json) => LabelDetails()
+  ..findImei = json['find_imei'] as String
+  ..getImeiInfo = json['get_imei_information'] as String
+  ..imeiDetails = json['imei_details'] as String
+  ..enterImei = json['enter_imei'] as String
+  ..optionA = json['option_a'] as String;
+
+Map<String, dynamic> _$LabelDetailsToJson(LabelDetails instance) =>
+    <String, dynamic>{
+      'find_imei': instance.findImei,
+      'get_imei_information': instance.getImeiInfo,
+      'imei_details': instance.imeiDetails,
+      'enter_imei': instance.enterImei,
+      'option_a': instance.optionA,
     };
