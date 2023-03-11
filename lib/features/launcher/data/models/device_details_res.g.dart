8030534cfef773879acab08221b606da2fa4cccd
dharmender <dharmender.manral@gmail.com>
2023-03-11 17:37:15 +0530

    modify label details json serialize class

--- lib/features/launcher/data/models/device_details_res.g.dart
+++ lib/features/launcher/data/models/device_details_res.g.dart
@@ -21,16 +21,98 @@ Map<String, dynamic> _$DeviceDetailsResToJson(DeviceDetailsRes instance) =>
 
 LabelDetails _$LabelDetailsFromJson(Map<String, dynamic> json) => LabelDetails()
   ..findImei = json['find_imei'] as String?
-  ..getImeiInfo = json['get_imei_information'] as String?
+  ..getImeiInfo = json['get_imie_information'] as String?
   ..imeiDetails = json['imei_details'] as String?
   ..enterImei = json['enter_imei'] as String?
-  ..optionA = json['option_a'] as String?;
+  ..optionA = json['option_a'] as String?
+  ..aboutUs = json['aboutUs'] as String?
+  ..makeSureWifiData = json['makeSureWifiData'] as String?
+  ..checkImeiMesssage = json['checkImeiMesssage'] as String?
+  ..imeiAlsoWrittenInBox =
+      json['IMEI_also_written_in_the_box_as_shown_in_Image_below'] as String?
+  ..imeiAlsoWritten = json['imeiAlsoWritten'] as String?
+  ..scan = json['scan'] as String?
+  ..enterFifteenDigit = json['enter15Digit'] as String?
+  ..changeAppLanguage = json['changeAppLanguage'] as String?
+  ..eirsApp = json['eirs_app'] as String?
+  ..remark = json['remark'] as String?
+  ..emailId = json['emailId'] as String?
+  ..language = json['language'] as String?
+  ..selectOneImei = json['selectOneImei'] as String?
+  ..callToAction = json['callToAction'] as String?
+  ..sorryUnableToFetch = json['sorryUnableToFetch'] as String?
+  ..getList = json['getList'] as String?
+  ..oops = json['oops'] as String?
+  ..ok = json['ok'] as String?
+  ..findImeiLabel = json['findImei'] as String?
+  ..dial = json['dial'] as String?
+  ..contactUs = json['contactUs'] as String?
+  ..or = json['or'] as String?
+  ..scanIMEI = json['scanIMEI'] as String?
+  ..clickToWatch = json['clickToWatch'] as String?
+  ..changeLanguage = json['changeLanguage'] as String?
+  ..knowMore = json['knowMore'] as String?
+  ..tryAgain = json['tryAgain'] as String?
+  ..noInternetConnection = json['noInternetConnection'] as String?
+  ..optionALabel = json['optionA'] as String?
+  ..history = json['history'] as String?
+  ..optionB = json['optionB'] as String?
+  ..checkIMEI = json['checkIMEI'] as String?
+  ..scanBar = json['scanBar'] as String?
+  ..eirsAppHeader = json['eirsAppHeader'] as String?
+  ..enterIMEI = json['enterIMEI'] as String?
+  ..canBeBarcode = json['canBeBarcode'] as String?
+  ..getIMEIInfo = json['getIMEIInfo'] as String?
+  ..invalid = json['invalid'] as String?
+  ..checkOtherImei = json['checkOtherImei'] as String?
+  ..needAnyHelp = json['needAnyHelp'] as String?;
 
 Map<String, dynamic> _$LabelDetailsToJson(LabelDetails instance) =>
     <String, dynamic>{
       'find_imei': instance.findImei,
-      'get_imei_information': instance.getImeiInfo,
+      'get_imie_information': instance.getImeiInfo,
       'imei_details': instance.imeiDetails,
       'enter_imei': instance.enterImei,
       'option_a': instance.optionA,
+      'aboutUs': instance.aboutUs,
+      'makeSureWifiData': instance.makeSureWifiData,
+      'checkImeiMesssage': instance.checkImeiMesssage,
+      'IMEI_also_written_in_the_box_as_shown_in_Image_below':
+          instance.imeiAlsoWrittenInBox,
+      'imeiAlsoWritten': instance.imeiAlsoWritten,
+      'scan': instance.scan,
+      'enter15Digit': instance.enterFifteenDigit,
+      'changeAppLanguage': instance.changeAppLanguage,
+      'eirs_app': instance.eirsApp,
+      'remark': instance.remark,
+      'emailId': instance.emailId,
+      'language': instance.language,
+      'selectOneImei': instance.selectOneImei,
+      'callToAction': instance.callToAction,
+      'sorryUnableToFetch': instance.sorryUnableToFetch,
+      'getList': instance.getList,
+      'oops': instance.oops,
+      'ok': instance.ok,
+      'findImei': instance.findImeiLabel,
+      'dial': instance.dial,
+      'contactUs': instance.contactUs,
+      'or': instance.or,
+      'scanIMEI': instance.scanIMEI,
+      'clickToWatch': instance.clickToWatch,
+      'changeLanguage': instance.changeLanguage,
+      'knowMore': instance.knowMore,
+      'tryAgain': instance.tryAgain,
+      'noInternetConnection': instance.noInternetConnection,
+      'optionA': instance.optionALabel,
+      'history': instance.history,
+      'optionB': instance.optionB,
+      'checkIMEI': instance.checkIMEI,
+      'scanBar': instance.scanBar,
+      'eirsAppHeader': instance.eirsAppHeader,
+      'enterIMEI': instance.enterIMEI,
+      'canBeBarcode': instance.canBeBarcode,
+      'getIMEIInfo': instance.getIMEIInfo,
+      'invalid': instance.invalid,
+      'checkOtherImei': instance.checkOtherImei,
+      'needAnyHelp': instance.needAnyHelp,
     };
