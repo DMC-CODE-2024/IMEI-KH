ffb510e3ce44f2f42428c568aed78efdcbb7d248
dharmender <dharmender.manral@gmail.com>
2023-03-30 08:39:51 +0530

    generate new config file

--- lib/features/launcher/data/models/device_details_req.g.dart
+++ lib/features/launcher/data/models/device_details_req.g.dart
@@ -11,8 +11,7 @@ DeviceDetailsReq _$DeviceDetailsReqFromJson(Map<String, dynamic> json) =>
       osType: json['osType'] as String,
       deviceId: json['deviceId'] as String,
       languageType: json['languageType'] as String,
-      deviceDetails: AndroidDeviceDetails.fromJson(
-          json['deviceDetails'] as Map<String, dynamic>),
+      deviceDetails: json['deviceDetails'],
     );
 
 Map<String, dynamic> _$DeviceDetailsReqToJson(DeviceDetailsReq instance) =>
@@ -21,6 +20,38 @@ Map<String, dynamic> _$DeviceDetailsReqToJson(DeviceDetailsReq instance) =>
       'deviceId': instance.deviceId,
       'languageType': instance.languageType,
       'deviceDetails': instance.deviceDetails,
+    };
+
+IosDeviceDetails _$IosDeviceDetailsFromJson(Map<String, dynamic> json) =>
+    IosDeviceDetails(
+      name: json['name'] as String?,
+      systemName: json['systemName'] as String?,
+      systemVersion: json['systemVersion'] as String?,
+      model: json['model'] as String?,
+      localizedModel: json['localizedModel'] as String?,
+      id: json['id'] as String?,
+      isPhysicalDevice: json['isPhysicalDevice'] as bool?,
+      sysname: json['sysname'] as String?,
+      nodename: json['nodename'] as String?,
+      release: json['release'] as String?,
+      version: json['version'] as String?,
+      machine: json['machine'] as String?,
+    );
+
+Map<String, dynamic> _$IosDeviceDetailsToJson(IosDeviceDetails instance) =>
+    <String, dynamic>{
+      'name': instance.name,
+      'systemName': instance.systemName,
+      'systemVersion': instance.systemVersion,
+      'model': instance.model,
+      'localizedModel': instance.localizedModel,
+      'id': instance.id,
+      'isPhysicalDevice': instance.isPhysicalDevice,
+      'sysname': instance.sysname,
+      'nodename': instance.nodename,
+      'release': instance.release,
+      'version': instance.version,
+      'machine': instance.machine,
     };
 
 AndroidDeviceDetails _$AndroidDeviceDetailsFromJson(
