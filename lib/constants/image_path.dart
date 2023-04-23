74a87eddb2f689da4a82528c03c62dc8c741ddd2
Dharmender Manral <dharmender.manral@gmail.com>
2023-04-23 22:42:47 +0530

    Remove unused resource

--- lib/constants/image_path.dart
+++ lib/constants/image_path.dart
@@ -1,13 +1,11 @@
 class ImageConstants {
   const ImageConstants._();
 
-  static const String appIcon ="assets/images/ic_eirs.svg";
   static const String timeIcon ="assets/images/ic_time.svg";
   static const String infoIcon ="assets/images/ic_info.svg";
   static const String localizationIcon ="assets/images/ic_localization.svg";
   static const String scanIcon ="assets/images/ic_transform.svg";
   static const String deviceInfo ="assets/images/ic_device_info.jpg";
-  static const String deviceBox ="assets/images/ic_device_box.jpg";
   static const String crossIcon ="assets/images/ic_cross.svg";
   static const String imeiValidIcon ="assets/images/ic_check_circle.svg";
   static const String imeiInValidIcon ="assets/images/ic_cross_circle.svg";
@@ -16,10 +14,8 @@ class ImageConstants {
   static const String helpIcon ="assets/images/ic_help.svg";
   static const String mobileOffIcon ="assets/images/ic_mobile_off.svg";
   static const String mobileOnIcon ="assets/images/ic_mobile_on.svg";
-  static const String coachLanguage ="assets/images/ic_language.svg";
   static const String splashIcon = "assets/images/ic_app_logo.png";
   static const String errorImg = "assets/images/ic_error.svg";
   static const String noInternet = "assets/images/ic_no_internet.jpg";
   static const String optionA = "assets/images/ic_option_a.jpg";
-  static const String loadingImg = "assets/images/ic_loading.gif";
 }
